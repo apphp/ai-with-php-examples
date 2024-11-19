453a96be3103e9c3663412666607d9bfad94c95d
Samuel Akopyan <samuel.akopyan@thrivedx.com>
2024-11-19 23:42:39 +0200

    Changes in PHP-ML classifier code

--- public/pages/neural-networks/simple-perceptron/phpml-simple-perceptron-code.php
+++ public/pages/neural-networks/simple-perceptron/phpml-simple-perceptron-code.php
@@ -21,17 +21,17 @@ $labels = $dataset->getTargets();
 // - 2 input nodes (study hours, previous score)
 // - 1 output node (pass/fail)
 // No hidden layers in between
-$mlp = new MLPClassifier(2, [0], ['fail', 'pass'], 10000);
+$classifier = new MLPClassifier(2, [0], ['fail', 'pass'], 10000);
 
 // Step 3: Train the Network
-$mlp->train($samples, $labels);
+$classifier->train($samples, $labels);
 
 // Step 4: Make Predictions
 $testSamples = [
     [6, 82],  // New student: 6 hours study, 82% previous score
     [1, 50],  // New student: 1 hour study, 50% previous score
 ];
-$predictions = $mlp->predict($testSamples);
+$predictions = $classifier->predict($testSamples);
 
 // Output predictions
 foreach ($predictions as $index => $prediction) {

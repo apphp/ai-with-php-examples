3a217e331cc6371caddf48ddd0d1339c39619d51
Samuel Akopyan <samuel.akopyan@thrivedx.com>
2025-01-25 00:13:01 +0200

    Updated error handling for different envs

--- public/include/global.php
+++ public/include/global.php
@@ -1,13 +1,14 @@
 <?php
 
 // Local
-error_reporting(E_ALL);
+//error_reporting(E_ALL);
 define("APP_PATH", "/var/www/");
 define("APP_SEO_LINKS", true);
 define("APP_URL", "http://localhost:8088/");
 define("APP_URL_DIR", '');
 
 // Prod
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED);
 //define("APP_PATH", "");
 //define("APP_SEO_LINKS", true);
 //define("APP_URL", "");
